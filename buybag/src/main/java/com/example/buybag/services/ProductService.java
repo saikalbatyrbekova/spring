68a2b2d986b874e28ce3b315cee5283801cdc79d
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2024-03-19 11:47:32 +0600

    Update ProductService.java

--- buybag/src/main/java/com/example/buybag/services/ProductService.java
+++ buybag/src/main/java/com/example/buybag/services/ProductService.java
@@ -9,24 +9,32 @@ import java.util.List;
 @Service
 public class ProductService {
     private List<Product> products = new ArrayList<>();
-    private long ID = 0;
-    {
-        products.add(new Product(++ID, "MICHAEL KORS bag", "Medium Saffiano Leather Satchel", 358, "New-York", "Michael Kors"));
-        products.add(new Product(++ID,"MICHAEL KORS bag", "Mila Small Metallic Leather Shoulder Bag", 258, "New-York", "Michael Kors"));
-    }
-    public List<Product> list() {
-        return products;
-    }
-    public void saveProduct(Product product){
-        product.setId(++ID);
-        products.add(product);
-    }
-    public void deleteProduct(Long id){
-        products.removeIf(product -> product.getId().equals(id));
+    private long idCounter = 0;
 
+    {
+        products.add(new Product(++idCounter, "MICHAEL KORS bag", "Medium Saffiano Leather Satchel", 358, "New-York", "Michael Kors"));
+        products.add(new Product(++idCounter, "MICHAEL KORS bag", "Mila Small Metallic Leather Shoulder Bag", 258, "New-York", "Michael Kors"));
     }
 
     public List<Product> listProducts() {
         return products;
+    }
+
+    public void saveProduct(Product product) {
+        product.setId(++idCounter);
+        products.add(product);
+    }
+
+    public void deleteProductById(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+    public Product getProductById(Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
     }
 }
