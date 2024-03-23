5bc48dd8855af266ad7814ec2b3a04bf53f2eda5
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2024-03-23 13:33:40 +0600

    Update ProductController.java

--- buybag/src/main/java/com/example/buybag/controllers/ProductController.java
+++ buybag/src/main/java/com/example/buybag/controllers/ProductController.java
@@ -1,40 +1,41 @@
-package com.example.buybag.controllers;
+    package com.example.buybag.controllers;
 
-import com.example.buybag.models.Product;
-import com.example.buybag.services.ProductService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+    import com.example.buybag.models.Product;
+    import com.example.buybag.services.ProductService;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-@RequiredArgsConstructor
-public class ProductController {
-    private final ProductService productService;
+    @Controller
+    @RequiredArgsConstructor
+    public class ProductController {
+        private final ProductService productService;
 
-    @GetMapping("/")
-    public String products(Model model) {
-        model.addAttribute("products", productService.listProducts());
-        return "products";
+        @GetMapping("/")
+        public String products(@RequestParam (name = "title", required = false) String title, Model model) {
+            model.addAttribute("products", productService.listProducts(title));
+            return "products";
+        }
+
+        @GetMapping("/product/{id}")
+        public String productInfo(@PathVariable long id, Model model) {
+            model.addAttribute("product", productService.getProductById(id));
+            return "product-info";
+        }
+
+        @PostMapping("/product/create")
+        public String createProduct(Product product){
+            productService.saveProduct(product);
+            return "redirect:/";
+        }
+
+        @PostMapping("/product/delete/{id}")
+        public String deleteProduct(@PathVariable Long id){
+            productService.deleteProductById(id);
+            return "redirect:/";
+        }
     }
-
-    @GetMapping("/product/{id}")
-    public String productInfo(@PathVariable long id, Model model) {
-        model.addAttribute("product", productService.getProductById(id));
-        return "product-info";
-    }
-
-    @PostMapping("/product/create")
-    public String createProduct(Product product){
-        productService.saveProduct(product);
-        return "redirect:/";
-    }
-
-    @PostMapping("/product/delete/{id}")
-    public String deleteProduct(@PathVariable Long id){
-        productService.deleteProductById(id);
-        return "redirect:/";
-    }
-}
